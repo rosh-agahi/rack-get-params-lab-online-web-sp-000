341cd6cb587d21ffe3fbb37fbac1b48c945e7a28
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-20 06:57:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,14 +24,14 @@ class Application
       else 
         resp.write "Your cart is empty"
       end
-      
-      
+    
     elsif req.path.match(/add/)
     
       add_item = req.params["item"]
       
       if @@items.include?(add_item)
         @@cart << add_item
+        resp.write "added #{add_item}"
       else 
           resp.write "We don't have that item"
       end
